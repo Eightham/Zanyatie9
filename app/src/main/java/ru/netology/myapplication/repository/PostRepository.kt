68be4db7173d575f371c9d04315cad3fbad51b7f
Eightham <vperedereev@mail.ru>
2024-03-19 23:19:07 +0300

    Занятие 9

---
+++ app/src/main/java/ru/netology/myapplication/repository/PostRepository.kt
@@ -0,0 +1,13 @@
+package ru.netology.myapplication.repository
+import androidx.lifecycle.LiveData
+import ru.netology.myapplication.dto.Post
+
+interface PostRepository {
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Int)
+    fun shareById(id: Int)
+    fun removeById(id: Int)
+    fun save(post: Post)
+
+
+}
